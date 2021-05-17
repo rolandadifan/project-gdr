400771c573d87b5b13d7cba18af0ba9dae2495f6
Joko <joiel@users.noreply.github.com>
2021-05-17 15:40:38 +0700

    add youtube video on landing page

--- resources/views/superadmin/pages/setting/edit.blade.php
+++ resources/views/superadmin/pages/setting/edit.blade.php
@@ -17,14 +17,22 @@
             @method('put')
             <div class="d-flex">
                 <div class="form-group mr-5" style="width: 400px;">
-                    <label for="">Value</label>
+                    <label for="value">Value</label>
+                    @if ($setting->key == 'landing-video-section-1')
+                    <input type="text" name="value" class="form-control" value="{{ $setting->value }}">
+                    hanya isi dengan id saja https://www.youtube.com/watch?v=<strong
+                        style="color:indianred">SZesNKBVG3U</strong>
+                    @else
                     <textarea name="value" id="article-edit" class="form-control" rows="10"
                         required>{{ $setting->value }}</textarea>
+                    @endif
                 </div>
             </div>
             <div class="form-group" style="width: 400px">
                 <label for="">Thumbnail</label>
+                @isset($setting->thumbnail)
                 <img src="{{ Storage::url($setting->thumbnail) }}" height="200px" />
+                @endisset
                 <input type="file" name="thumbnail" id="" class="form-control">
                 <span style="font-size: 16px">Maksimal file 300kb</span>
             </div>
