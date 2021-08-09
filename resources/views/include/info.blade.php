4a7a82773fd8039fc50228da3c6ff32c7debec1f
rolandadifandana <rolanda.difandana@adira.co.id>
2021-08-09 10:52:42 +0700

    add admin link

--- resources/views/include/info.blade.php
+++ resources/views/include/info.blade.php
@@ -3,5 +3,10 @@
         <a href="http://staffsite.gunadarma.ac.id/" target="_blank">Staffsite</a>
         <a href="https://studentsite.gunadarma.ac.id/" target="_blank">Studensite</a>
         <a href="http://career.gunadarma.ac.id/" target="_blank">CareerCenter</a>
+        @auth
+            @if (auth()->user()->role != 'student')
+                <a href="http://international.gunadarma.ac.id/web/admin/" target="_blank">AdminSite</a>
+            @endif
+        @endauth
     </div>
 </div>
